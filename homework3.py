8feaed62f4ef4ff540becd5f8ca56d94f908ea2f
MPyanykh <ronin25rw@gmail.com>
2024-11-18 05:44:50 +0200

    Add files via upload

---
+++ homework3.py
@@ -0,0 +1,5 @@
+number_of_homeworks = 12
+time_spent = 1.5
+course = 'Python'
+time_per_task = time_spent / number_of_homeworks
+print('Курс:', course, ', всего задач:', number_of_homeworks, ', затрачено часов:', time_spent, ', среднее время выполнения', time_per_task, ' часа.')
